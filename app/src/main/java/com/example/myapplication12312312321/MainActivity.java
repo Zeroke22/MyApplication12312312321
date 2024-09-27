942a4c3caa83e272e9d497e5bc0f673c1caf9129
mykhailo <mykhailo@mykhailos-MacBook-Pro.local>
2024-09-27 23:17:47 +0100

    Added button to change text color

--- app/src/main/java/com/example/myapplication12312312321/MainActivity.java
+++ app/src/main/java/com/example/myapplication12312312321/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.myapplication12312312321;
 
 import android.os.Bundle;
-import android.widget.TextView;
 import android.view.View;
-
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -23,9 +22,16 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
-    public void onBtnChangeTextClick(View view){
-        TextView tvChangeText = (TextView)findViewById(R.id.tvChangeText);
-        tvChangeText.setText("Hello00000000");
 
+    // Method to handle button click to change text
+    public void onBtnChangeTextClick(View view) {
+        TextView tvChangeText = findViewById(R.id.tvChangeText);
+        tvChangeText.setText("Hello00000000");
+    }
+
+    // Method to handle button click to change text color
+    public void onBtnChangeColorClick(View view) {
+        TextView tvChangeText = findViewById(R.id.tvChangeText);
+        tvChangeText.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
     }
 }
